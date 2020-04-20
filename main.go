1417fb07a32c9559321d154444fc017c60e55136
zhiyong.dai <zhiyong.dai@ucloud.cn>
2020-04-20 17:55:49 +0800

    exec git pull

--- main.go
+++ main.go
@@ -1,23 +1,28 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
 	"log"
 	"net/http"
+	"os/exec"
 
 	"go.uber.org/zap"
 )
 
+var logger *zap.Logger
+
 func push(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("request: ")
-	s, _ := json.Marshal(r)
-	fmt.Println(string(s))
+	logger.Info("call push func")
+	cmd := exec.Command("cd screw && git pull")
+	err := cmd.Run()
+	if err != nil {
+		logger.Error(err.Error())
+	}
+
 }
 
 func main() {
 	// 设置路由，如果访问/，则调用index方法
-	logger, _ := zap.NewDevelopment()
+	logger, _ = zap.NewDevelopment()
 	logger.Info("webhook start at 47.101.47.118:8080")
 	http.HandleFunc("/push", push)
 
